942a2c81b4e2d96e291d5fda4ff418fbcb31ddee
e0035165 <eeenginmaster95@gmail.com>
2023-05-18 13:25:19 +0800

    Initial Commit

---
+++ TaskScheduler/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TaskScheduler
+//
+//  Created by Sart Way on 18/5/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
